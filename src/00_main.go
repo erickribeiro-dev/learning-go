9ac9d95b24dc78c66886eb7f0d3f40341ee94543
Erick Ribeiro <erickribeiro.dev@gmail.com>
2024-11-23 03:58:31 -0300

    Update 00_main.go
    More comments

--- src/00_main.go
+++ src/00_main.go
@@ -3,5 +3,7 @@ package main
 func main() {
 	// To test any of the files, type the function name here.
 	// For example, variables() from the file 01_variables.go
+	// or web_server_static_files() from the file 54_web_server_static_files.go
 	variables()
+	// web_server_static_files()
 }
